e3daabec487b4c983918e2a6d1eba3ffb17c98cf
Ginura Premawardana <ginujj9@gmail.com>
2024-04-21 15:09:47 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -1,242 +1,3 @@
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.*;
-//
-//class Cell {
-//    int row;
-//    int col;
-//    char type;
-//    List<Cell> neighbors;
-//
-//    public Cell(int row, int col, char type) {
-//        this.row = row;
-//        this.col = col;
-//        this.type = type;
-//        this.neighbors = new ArrayList<>();
-//    }
-//
-//    public void addNeighbor(Cell neighbor) {
-//        this.neighbors.add(neighbor);
-//    }
-//
-//}
-//
-//class MapGraph {
-//    private Cell[][] grid;
-//
-//    public MapGraph(String filename) {
-//        try {
-//            // Read map from file and initialize grid
-//            BufferedReader reader = new BufferedReader(new FileReader(filename));
-//            List<char[]> lines = new ArrayList<>();
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                lines.add(line.toCharArray());
-//            }
-//            reader.close();
-//
-//
-//            // Initialize grid
-//            int rows = lines.size();
-//            int cols = lines.get(0).length;
-//            grid = new Cell[rows][cols];
-//            for (int i = 0; i < rows; i++) {
-//                for (int j = 0; j < cols; j++) {
-//                    grid[i][j] = new Cell(i, j, lines.get(i)[j]);
-//                }
-//            }
-//
-//            System.out.println("");
-//
-//            for (Cell[] row : grid) {
-//                for (Cell cell : row) {
-//                    System.out.print(cell.type);
-//                }
-//                System.out.println();
-//            }
-//
-////            connectCells();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        MapGraph mapGraph = new MapGraph("example_map.txt");
-//    }
-//}
-
-
-
-//
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.*;
-//
-//class Cell {
-//    int row;
-//    int col;
-//    char type;
-//    List<Cell> neighbors;
-//
-//    public Cell(int row, int col, char type) {
-//        this.row = row;
-//        this.col = col;
-//        this.type = type;
-//        this.neighbors = new ArrayList<>();
-//    }
-//
-//    public void addNeighbor(Cell neighbor) {
-//        this.neighbors.add(neighbor);
-//    }
-//}
-//
-//class MapGraph {
-//    private Cell[][] grid;
-//    private int rows;
-//    private int cols;
-//
-//    public MapGraph(String filename) {
-//        try {
-//            // Read map from file and initialize grid
-//            BufferedReader reader = new BufferedReader(new FileReader(filename));
-//            List<char[]> lines = new ArrayList<>();
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                lines.add(line.toCharArray());
-//            }
-//            reader.close();
-//
-//            // Initialize grid
-//            rows = lines.size();
-//            cols = lines.get(0).length;
-//            grid = new Cell[rows][cols];
-//            for (int i = 0; i < rows; i++) {
-//                for (int j = 0; j < cols; j++) {
-//                    grid[i][j] = new Cell(i, j, lines.get(i)[j]);
-//                }
-//            }
-//
-//            connectCells();
-//            findShortestPath();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private void connectCells() {
-//        // Connect each cell with its neighbors
-//        for (int i = 0; i < rows; i++) {
-//            for (int j = 0; j < cols; j++) {
-//                if (grid[i][j].type != '0') {
-//                    // Add neighbors (up, down, left, right)
-//                    if (i > 0 && grid[i - 1][j].type != '0') // Up
-//                        grid[i][j].addNeighbor(grid[i - 1][j]);
-//                    if (i < rows - 1 && grid[i + 1][j].type != '0') // Down
-//                        grid[i][j].addNeighbor(grid[i + 1][j]);
-//                    if (j > 0 && grid[i][j - 1].type != '0') // Left
-//                        grid[i][j].addNeighbor(grid[i][j - 1]);
-//                    if (j < cols - 1 && grid[i][j + 1].type != '0') // Right
-//                        grid[i][j].addNeighbor(grid[i][j + 1]);
-//                }
-//            }
-//        }
-//    }
-//
-//    private void findShortestPath() {
-//        Queue<Cell> queue = new LinkedList<>();
-//        Map<Cell, Cell> parentMap = new HashMap<>();
-//        Set<Cell> visited = new HashSet<>();
-//
-//        Cell startCell = null;
-//        Cell finishCell = null;
-//
-//        // Find start and finish cells
-//        for (int i = 0; i < rows; i++) {
-//            for (int j = 0; j < cols; j++) {
-//                if (grid[i][j].type == 'S') startCell = grid[i][j];
-//                if (grid[i][j].type == 'F') finishCell = grid[i][j];
-//            }
-//        }
-//
-//        // BFS
-//        queue.offer(startCell);
-//        visited.add(startCell);
-//        parentMap.put(startCell, null);
-//
-//        while (!queue.isEmpty()) {
-//            Cell current = queue.poll();
-//
-//            // If finish reached, stop BFS
-//            if (current == finishCell) break;
-//
-//            for (Cell neighbor : current.neighbors) {
-//                if (!visited.contains(neighbor)) {
-//                    if (neighbor.type == '.') {
-//                        // If neighbor is ice, continue sliding
-//                        Cell next = getNextCellInDirection(current, neighbor);
-//                        if (next == current || next == finishCell) {
-//                            neighbor = next;
-//                        }
-//                    }
-//                    queue.offer(neighbor);
-//                    visited.add(neighbor);
-//                    parentMap.put(neighbor, current);
-//                }
-//            }
-//        }
-//
-//        // Reconstruct path if finish reached
-//        if (parentMap.containsKey(finishCell)) {
-//            List<Cell> path = new ArrayList<>();
-//            Cell current = finishCell;
-//            while (current != null) {
-//                path.add(current);
-//                current = parentMap.get(current);
-//            }
-//            Collections.reverse(path);
-//
-//            // Output the steps of the solution
-//            int step = 1;
-//            for (Cell cell : path) {
-//                System.out.println(step + ". Move to (" + (cell.row + 1) + "," + (cell.col + 1) + ")");
-//                step++;
-//            }
-//            System.out.println("Done!");
-//        } else {
-//            System.out.println("No path found to the finish.");
-//        }
-//    }
-//
-//    private Cell getNextCellInDirection(Cell current, Cell iceStart) {
-//        int dx = iceStart.col - current.col;
-//        int dy = iceStart.row - current.row;
-//        Cell next = iceStart;
-//        while (true) {
-//            int nextRow = next.row + dy;
-//            int nextCol = next.col + dx;
-//            if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols || grid[nextRow][nextCol].type == '0') {
-//                break; // Hit wall
-//            }
-//            if (grid[nextRow][nextCol].type == 'F') {
-//                return grid[nextRow][nextCol]; // Reached finish
-//            }
-//            next = grid[nextRow][nextCol];
-//        }
-//        return next;
-//    }
-//
-//    public static void main(String[] args) {
-//        MapGraph mapGraph = new MapGraph("example_map.txt");
-//    }
-//}
-
-
-
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -338,7 +99,6 @@ class MapGraph {
         Queue<Cell> queue = new LinkedList<>();
         Map<Cell, Cell> parentMap = new HashMap<>();
         Set<Cell> visited = new HashSet<>();
-
         queue.offer(startCell);
         visited.add(startCell);
 
@@ -346,19 +106,15 @@ class MapGraph {
             Cell currentCell = queue.poll();
 
             if (currentCell.type == 'F') {
-                // Found the finish cell, reconstruct the path
                 List<Cell> path = reconstructPath(parentMap, currentCell);
                 printPath(path);
                 return;
             }
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-            // Explore the neighbors in all four directions
-            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Up, Down, Left, Right
             for (int[] direction : directions) {
                 int newRow = currentCell.row;
                 int newCol = currentCell.col;
-
-                // Slide in the current direction until hitting a wall or rock
                 while (isValidCell(newRow + direction[0], newCol + direction[1])) {
                     newRow += direction[0];
                     newCol += direction[1];
@@ -366,7 +122,6 @@ class MapGraph {
                         break;
                     }
                 }
-
                 Cell neighbor = grid[newRow][newCol];
                 if (!visited.contains(neighbor)) {
                     queue.offer(neighbor);
@@ -375,7 +130,6 @@ class MapGraph {
                 }
             }
         }
-
         System.out.println("No path found from 'S' to 'F'.");
     }
 
